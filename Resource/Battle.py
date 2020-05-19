33dad513663e688cf7114bc5fd8e2a30ee8d342e
65wu <904795395@qq.com>
2020-05-20 01:08:41 +0800

    :sparkles: 初步完成战斗函数，并补充玩家测试类

--- Resource/Battle.py
+++ Resource/Battle.py
@@ -8,7 +8,7 @@ class Round:
         self.player = player
         self.monster = monster
 
-    def round_info(self):
+    def info(self):
         """
         返回玩家和怪物的基本信息
         :return:
@@ -20,7 +20,7 @@ class Round:
             "monster_info": monster_info
         }
 
-    def round_attack_sequence(self):
+    def attack_sequence(self):
         """
         获取本回合玩家和怪物的攻击先后顺序
         :return:
@@ -31,7 +31,7 @@ class Round:
         sorted(order_list, key=lambda role: role.speed)
         return order_list
 
-    def round_rest(self):
+    def rest(self):
         """
         回合结束，在体力都大于0的前提下，玩家和怪物同时回复魔法值
         :return:
@@ -39,7 +39,7 @@ class Round:
         self.player.recover()
         self.monster.recover()
 
-    def round_end_check(self, role):
+    def end_check(self, role):
         """
         判断回合是否应该终止
         :param role: 即将执行攻击的角色
@@ -49,18 +49,18 @@ class Round:
             if type(role) == Player:
                 return {
                     "battle_state": Battle_state.lose,
-                    "round_info": self.round_info()
+                    "round_info": self.info()
                 }
             # 暂时一对一，一个怪死了就算胜利
             else:
                 return {
                     "battle_state": Battle_state.win,
-                    "round_info": self.round_info()
+                    "round_info": self.info()
                 }
         else:
             return None
 
-    def round_exec(self, skill_id):
+    def exec(self, skill_id):
         """
         单回合执行函数
         :param skill_id: 用户选择的技能
@@ -69,11 +69,11 @@ class Round:
         player = self.player
         monster = self.monster
         player_attack_damage = monster_attack_damage = 0
-        order_list = self.round_attack_sequence()
+        order_list = self.attack_sequence()
 
         # 依照速度排序，依次执行攻击
         for role in order_list:
-            if_round_end = self.round_end_check(role)
+            if_round_end = self.end_check(role)
             # 检查是否有角色死亡
             if if_round_end:
                 return if_round_end
@@ -85,10 +85,10 @@ class Round:
                     monster_attack_damage = monster.attack(player)["damage"]
 
         # 回合继续，双方回复mp
-        self.round_rest()
+        self.rest()
         return {
             "battle_state": Battle_state.be_in_progress,
-            "round_info": self.round_info(),
+            "round_info": self.info(),
             "damage": {
                 "by_player": player_attack_damage,
                 "by_monster": monster_attack_damage
@@ -106,6 +106,46 @@ class Battle:
     def __init__(self, player, monster):
         self.player = player
         self.monster = monster
+        self.game_round = Round(player, monster)
+
+    def start(self):
+        game_round = self.game_round
+        skill_id = input("请输入使用的技能id: ")
+        round_result = game_round.exec(skill_id)
+
+        while round_result["battle_state"] == Battle_state.be_in_progress:
+            print(f"""
+            当前回合为{game_round},
+            {self.player.name}对{self.monster.name}造成了{round_result["damage"]["by_player"]}
+            {self.monster.name}对{self.player.name}造成了{round_result["damage"]["by_monster"]}
+
+            目前{self.player.name}
+            血量为{round_result["round_info"]["player_info"]["hp_current"]},
+            魔法量为{round_result["round_info"]["player_info"]["mp_current"]}
+
+            {self.monster.name}
+            血量为{round_result["round_info"]["monster_info"]["hp_current"]},
+            魔法量为{round_result["round_info"]["monster_info"]["mp_current"]}
+            """)
+            game_round += 1
+
+        end_map = {
+            -1: "战斗失败",
+            1: "战斗胜利"
+        }
+
+        print(f"""
+        当前回合为{game_round},
+        {end_map[round_result["battle_state"]]}
+
+        当前{self.player.name}
+        血量为{round_result["round_info"]["player_info"]["hp_current"]},
+        魔法量为{round_result["round_info"]["player_info"]["mp_current"]}
+
+        {self.monster.name}
+        血量为{round_result["round_info"]["monster_info"]["hp_current"]},
+        魔法量为{round_result["round_info"]["monster_info"]["mp_current"]}
+        """)
 
 
 def test():
@@ -135,4 +175,23 @@ def test():
             Skill("头锥", 30, 15)
         ]
     )
-    battle = Battle(monster_a, monster_b)
+
+    player_test = Player(
+        name="史莱姆",
+        level=1,
+        exp=0,
+        hp=500,
+        mp=100,
+        attack=10,
+        defence=10,
+        speed=3,
+        skill_list=[
+            Skill("粘液喷吐", 10, 5),
+            Skill("肉弹冲击", 25, 15)
+        ]
+    )
+    battle = Battle(player_test, monster_b)
+    battle.start()
+
+
+test()
