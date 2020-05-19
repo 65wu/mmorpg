4bf8a689cfdf4e2c20d648fc692fa7bb94e2e8f9
65wu <904795395@qq.com>
2020-05-20 01:22:40 +0800

    :pencil2: 状态信息输入错误

--- Resource/Battle.py
+++ Resource/Battle.py
@@ -100,7 +100,7 @@ def battle_state_detail(state_code):
     detail_map = {
         Battle_state.lose: "战斗失败",
         Battle_state.be_in_progress: "战斗进行中",
-        Battle_state.win: "战斗失败"
+        Battle_state.win: "战斗胜利"
     }
     return detail_map[state_code]
 
