f6118b31cc0f6cfe49fc551226dca508f94cab9a
65wu <904795395@qq.com>
2020-05-20 11:49:18 +0800

    :children_crossing: 封装一下回合打印信息

--- Resource/Battle.py
+++ Resource/Battle.py
@@ -10,29 +10,8 @@ class Battle:
         self.monster = monster
         self.game_round = Round(player, monster)
 
-    def start(self):
+    def round_status_print(self, round_result):
         game_round = self.game_round
-        skill_id = int(input("请输入使用的技能id: "))
-        round_result = game_round.exec(skill_id)
-
-        while round_result["battle_state"] == Battle_state.be_in_progress:
-            print(f"""
-            {game_round},
-            {self.player}对{self.monster}造成了{round_result["damage"]["by_player"]}伤害
-            {self.monster}对{self.player}造成了{round_result["damage"]["by_monster"]}伤害
-
-            目前{self.player}
-            血量为{round_result["round_info"]["player_info"]["hp_current"]},
-            魔法量为{round_result["round_info"]["player_info"]["mp_current"]}
-
-            {self.monster}
-            血量为{round_result["round_info"]["monster_info"]["hp_current"]},
-            魔法量为{round_result["round_info"]["monster_info"]["mp_current"]}
-            """)
-            game_round.count += 1
-            skill_id = int(input("请输入使用的技能id: "))
-            round_result = game_round.exec(skill_id)
-
         print(f"""
         {game_round},
         {battle_state_detail(round_result["battle_state"])}
@@ -45,6 +24,26 @@ class Battle:
         血量为{round_result["round_info"]["monster_info"]["hp_current"]},
         魔法量为{round_result["round_info"]["monster_info"]["mp_current"]}
         """)
+
+    def round_damage_print(self, round_result):
+        print(f"""
+        {self.player}对{self.monster}造成了{round_result["damage"]["by_player"]}伤害
+        {self.monster}对{self.player}造成了{round_result["damage"]["by_monster"]}伤害
+        """)
+
+    def start(self):
+        game_round = self.game_round
+        skill_id = int(input("请输入使用的技能id: "))
+        round_result = game_round.exec(skill_id)
+
+        while round_result["battle_state"] == Battle_state.be_in_progress:
+            self.round_status_print(round_result)
+            self.round_damage_print(round_result)
+            game_round.count += 1
+            skill_id = int(input("请输入使用的技能id: "))
+            round_result = game_round.exec(skill_id)
+
+        self.round_status_print(round_result)
 
 
 def test():
