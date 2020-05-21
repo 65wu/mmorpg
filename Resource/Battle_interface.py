e10f4ab79fc0e242a2036888945fbcaae4ff980e
65wu <904795395@qq.com>
2020-05-21 21:37:38 +0800

    :tada: 战斗界面初始化

--- Resource/Battle_interface.py
+++ Resource/Battle_interface.py
@@ -3,14 +3,14 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode([500, 500])
-screen.fill((255, 255, 255))
-running = True
-while running:
+pygame.display.set_caption("史莱姆大战勇士")
+
+run = True
+
+while run:
+    pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-    pygame.display.flip()
+            run = False
 
 pygame.quit()
