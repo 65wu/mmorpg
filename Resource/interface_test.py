bce50a27140ba8cad5a88cd97acfc9fa5c9809f1
65wu <904795395@qq.com>
2020-05-26 01:35:25 +0800

    :building_construction: 封装一下属性

--- Resource/interface_test.py
+++ Resource/interface_test.py
@@ -15,32 +15,45 @@ player_dir = "/Image/Role/slime.png"
 def load_image():
     bg = pygame.image.load(data_dir + bg_dir)
 
+    role_size = 300, 300
+    role_y = 160
+    monster_x = 0
+    player_x = 500
+
     monster_source = pygame.image.load(data_dir + monster_dir)
-    monster = pygame.transform.scale(monster_source, (300, 300))
+    monster = pygame.transform.scale(monster_source, role_size)
 
     player_source = pygame.image.load(data_dir + player_dir)
-    player_scale = pygame.transform.scale(player_source, (300, 300))
+    player_scale = pygame.transform.scale(player_source, role_size)
     player = pygame.transform.flip(player_scale, True, False)
 
     screen.blit(bg, (0, 0))
-    screen.blit(monster, (0, 160))
-    screen.blit(player, (500, 160))
+    screen.blit(monster, (monster_x, role_y))
+    screen.blit(player, (player_x, role_y))
 
 
 def load_text():
-    font = pygame.font.SysFont("SimHei", 35)
+    font_size = 35
+    font = pygame.font.SysFont("SimHei", font_size)
     monster_name = font.render("骑士", True, (0, 0, 0))
     player_name = font.render("史莱姆", True, (0, 0, 0))
 
+    monster_text_x = 20
+    player_text_x = 500 + monster_text_x
+    role_text_y = 70
+
     # 角色名称
-    screen.blit(monster_name, (20, 70))
-    screen.blit(player_name, (520, 70))
+    screen.blit(monster_name, (monster_text_x, role_text_y))
+    screen.blit(player_name, (player_text_x, role_text_y))
 
-
-def load_hp_and_mp():
     # 血条和蓝条
-    pygame.draw.rect(screen, (0, 255, 0), (20, 120, 250, 25))
-    pygame.draw.rect(screen, (0, 0, 255), (20, 150, 250, 25))
+    rect_width = 250
+    rect_height = 25
+    hp_y = 120
+    mp_y = int(hp_y + rect_height * 6 / 5)
+
+    pygame.draw.rect(screen, (0, 255, 0), (monster_text_x, hp_y, rect_width, rect_height))
+    pygame.draw.rect(screen, (0, 0, 255), (monster_text_x, mp_y, rect_width, rect_height))
 
 
 if __name__ == '__main__':
@@ -48,12 +61,11 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                skill_id = 1
-                print(f"图形界面{skill_id=}")
+            # keys = pygame.key.get_pressed()
+            # if keys[pygame.K_LEFT]:
+            #     skill_id = 1
+            #     print(f"图形界面{skill_id=}")
 
         load_image()
         load_text()
-        load_hp_and_mp()
         pygame.display.update()
