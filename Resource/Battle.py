358bc031d95f96c4ad59289a2268d7a9fd90967d
65wu <904795395@qq.com>
2020-06-02 15:46:12 +0800

    :pencil: 补充文档

--- Resource/Battle.py
+++ Resource/Battle.py
@@ -13,7 +13,10 @@ from Model.Logic.Skill import Skill
 
 
 def _async_raise(tid, exec_type):
-    """raises the exception, performs cleanup if needed"""
+    """
+    杀死线程
+    raises the exception, performs cleanup if needed
+    """
     tid = ctypes.c_long(tid)
     if not inspect.isclass(exec_type):
         exec_type = type(exec_type)
@@ -34,58 +37,94 @@ def stop_thread(thread):
 class Battle:
     class Battle_interface:
         def __init__(self, player, monster, battle):
+            """
+            战斗逻辑层初始化
+            :param player: 玩家
+            :param monster: 怪物
+            :param battle: 战斗类，方便拿外部类的公有属性
+            """
             self.player = player
             self.monster = monster
             self.battle = battle
 
         def start(self):
+            """
+            逻辑启动
+            :return: 无
+            """
+            # 初始化pygame，我塞__init__里会爆bug，拿不到处理过的pygame, 不晓得为什么
             run = True
             pygame.init()
 
+            # 初始化屏幕
             screen = pygame.display.set_mode([800, 600])
             pygame.display.set_caption("史莱姆大战勇士")
 
+            # 初始化按钮类、图片类和信息类
             button = Button(pygame, screen, self.player)
             image = Image(pygame, screen, self.monster.image, self.player.image)
             info = Info(pygame, screen, self.monster.basic_info(), self.player.basic_info())
 
+            # pygame事件循环
             while run:
                 for event in pygame.event.get():
+                    # 如果点右上角，设定循环条件为否
                     if event.type == pygame.QUIT:
                         run = False
+                    # 如果有点击事件
                     if event.type == pygame.MOUSEBUTTONDOWN:
+                        # 调用按钮类的检查函数，会去判断点击的坐标是不是在按钮上，并判断点的是哪个技能按钮
                         tmp_skill_id = button.check_button_coordinate(event)
+                        # 如果点的是按钮
                         if tmp_skill_id is not None:
                             self.battle.skill_id = tmp_skill_id
+                            # 释放逻辑层的技能等待锁
                             self.battle.skill_choose_event.set()
 
+                            # 重置本轮信息锁为锁定，并等待逻辑层响应
                             self.battle.round_info_event.clear()
                             self.battle.round_info_event.wait()
 
+                            # 拿到逻辑层信息后，更新血条蓝条
                             info.update_info(
                                 self.battle.round_info["monster_info"],
                                 self.battle.round_info["player_info"]
                             )
+                            # 更新技能状况，蓝不够的技能会变灰
                             button.update_button_list(self.player)
+
                 image.load_image()
                 info.load_text()
                 button.load_button()
                 pygame.display.update()
 
             pygame.quit()
+            # 用户主动关闭窗口，先释放逻辑层的锁，再杀死逻辑线程
             self.battle.skill_choose_event.set()
             stop_thread(self.battle.logic)
 
     class Battle_logic:
         def __init__(self, player, monster, battle):
+            """
+            逻辑层初始化
+            :param player: 玩家对象
+            :param monster: 怪物对象
+            :param battle: 战斗类，拿公有属性和方法
+            """
             self.player = player
             self.monster = monster
             self.battle = battle
 
+            # 初始化round类，并设定战斗结果为-2，意味着异常退出
             self.game_round = Round(player, monster)
             self._result = -2
 
         def round_status_print(self, round_result):
+            """
+            本轮信息log打印函数
+            :param round_result:
+            :return:
+            """
             game_round = self.game_round
             print(f"""
             {game_round},
@@ -101,61 +140,98 @@ class Battle:
             """)
 
         def round_damage_print(self, round_result):
+            """
+            伤害打印函数
+            :param round_result:
+            :return:
+            """
             print(f"""
             {self.player}对{self.monster}造成了{round_result["damage"]["by_player"]}伤害
             {self.monster}对{self.player}造成了{round_result["damage"]["by_monster"]}伤害
             """)
 
         def start(self):
+            """
+            逻辑层启动
+            :return:
+            """
 
             game_round = self.game_round
             round_result = dict()
             round_result["battle_state"] = Battle_state.be_in_progress
 
+            # 如果游戏未结束，则继续循环
             while round_result["battle_state"] == Battle_state.be_in_progress:
+                # 第一轮不打印信息
                 if game_round.count:
                     self.round_status_print(round_result)
                     self.round_damage_print(round_result)
                 game_round.count += 1
 
+                # 重置技能选择锁，等待用户在界面上的操作
                 self.battle.skill_choose_event.clear()
                 self.battle.skill_choose_event.wait()
 
+                # 技能选择锁释放后，计算这轮的伤害和角色当前信息，信息是储存在外部公有属性里的
                 round_result = game_round.exec(self.battle.skill_id)
                 self.battle.round_info = round_result["round_info"]
+                # 释放本轮信息锁
                 self.battle.round_info_event.set()
 
+            # 战斗结束，打印最终结果，并休眠两秒，以免退出太快看不清
             self.round_status_print(round_result)
             time.sleep(2)
-            stop_thread(self.battle.interface)
+            # 更新battle_state信息
             self._result = round_result["battle_state"]
 
+            stop_thread(self.battle.interface)
+
         def get_result(self):
+            """
+            获取战斗结果，以调用exp函数
+            :return:
+            """
             return battle_state_detail(self._result)
 
     def __init__(self, player, monster):
+        """
+        战斗类初始化，负责储藏公有属性，和初始化逻辑层和界面层线程
+        :param player: 玩家对象
+        :param monster: 怪物对象
+        """
         self.player = player
         self.monster = monster
 
+        # 初始化两把进程锁
         self.skill_choose_event = threading.Event()
         self.round_info_event = threading.Event()
+        # 技能id和战斗信息，作为公有信息，因为两个线程都需要使用
         self.skill_id = 0
         self.round_info = {}
 
+        # 初始化两个类
         self.battle_logic = self.Battle_logic(self.player, self.monster, self)
         self.battle_interface = self.Battle_interface(self.player, self.monster, self)
+        # 初始化它们的线程
         self.logic = threading.Thread(target=self.battle_logic.start)
         self.interface = threading.Thread(target=self.battle_interface.start)
 
     def run(self):
+        # 线程启动
         self.logic.start()
         self.interface.start()
+        # 阻塞主线程
         self.logic.join()
         self.interface.join()
+        # 返回战斗结果状态码
         return self.battle_logic.get_result()
 
 
 def test():
+    """
+    测试方法
+    :return:
+    """
     monster_test = Monster(
         name="骑士",
         level=1,
